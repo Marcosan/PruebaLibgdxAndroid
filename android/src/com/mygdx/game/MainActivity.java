d1fff16506c9c0d9c48f305906b800fede75fd32
maxamend <marcoxavibsc@gmail.com>
2017-04-19 02:46:29 -0500

    Prueba de funcionamiento de cambio de Activities, basico

--- android/src/com/mygdx/game/MainActivity.java
+++ android/src/com/mygdx/game/MainActivity.java
@@ -1,14 +1,28 @@
 package com.mygdx.game;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Button btnPrueba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPrueba = (Button) findViewById(R.id.button1);
+
+        btnPrueba.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
